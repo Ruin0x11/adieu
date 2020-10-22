7e320bec4abcf2f83dd927c495f42c16e9996190
Ruin0x11 <ipickering2@gmail.com>
2020-10-22 01:27:55 -0700

    Add disassembler frontend

--- adieu/src/main.rs
+++ adieu/src/main.rs
@@ -8,14 +8,19 @@ extern crate serde_lexpr;
 extern crate anyhow;
 #[macro_use] extern crate log;
 extern crate env_logger;
+extern crate clap;
 
 #[cfg(test)]
 extern crate pretty_assertions;
 
 mod disasm;
 
-use std::fs;
+use std::fs::{self, File};
+use std::io::Write;
+use std::path::Path;
 use anyhow::Result;
+use clap::{Arg, App, SubCommand};
+use avg32::write::Writeable;
 
 #[allow(dead_code)]
 fn readall() -> Result<()> {
@@ -33,12 +38,80 @@ fn readall() -> Result<()> {
     Ok(())
 }
 
+fn get_app<'a, 'b>() -> App<'a, 'b> {
+    App::new("My Super Program")
+        .version("1.0")
+        .author("Kevin K. <kbknapp@gmail.com>")
+        .about("Does awesome things")
+        .subcommand(SubCommand::with_name("disasm")
+                    .about("Disassemble an AVG32 scene")
+                    .arg(Arg::with_name("output-dir")
+                         .short("o")
+                         .long("output-dir")
+                         .help("output directory")
+                         .takes_value(true)
+                         .value_name("DIR"))
+                    .arg(Arg::with_name("FILE")
+                         .required(true)
+                         .help("SEEN<nnn>.txt file")
+                         .index(1))
+        )
+        .subcommand(SubCommand::with_name("asm")
+                    .about("Assemble an adieu disassembly into an AVG32 scene")
+                    .arg(Arg::with_name("output-dir")
+                         .short("o")
+                         .long("output-dir")
+                         .help("output directory")
+                         .takes_value(true)
+                         .value_name("DIR"))
+                    .arg(Arg::with_name("FILE")
+                         .required(true)
+                         .help("SEEN<nnn>.adieu file")
+                         .index(1)))
+}
+
+
 fn main() -> Result<()> {
     env_logger::init();
 
-    // readall()?;
-    let scene = avg32::load(&"SEEN/SEEN020.TXT").unwrap();
-    println!("{}", disasm::disassemble(&scene)?);
+    let app = get_app();
+    let matches = app.get_matches();
+
+    match matches.subcommand() {
+        ("disasm", Some(sub_matches)) => {
+            let input_file = Path::new(sub_matches.value_of("FILE").unwrap());
+            let output_dir = match sub_matches.value_of("output-dir") {
+                Some(dir) => Path::new(dir),
+                None => input_file.parent().unwrap()
+            };
+            let output_file = output_dir.join(input_file.with_extension("adieu").file_name().unwrap());
+
+            let scene = avg32::load(&input_file.to_str().unwrap())?;
+            let sexp = disasm::disassemble(&scene)?;
+
+            let mut file = File::create(&output_file)?;
+            file.write_all(&sexp.as_bytes())?;
+
+            println!("Wrote to {:?}.", output_file);
+        },
+        ("asm", Some(sub_matches)) => {
+            let input_file = Path::new(sub_matches.value_of("FILE").unwrap());
+            let output_dir = match sub_matches.value_of("output-dir") {
+                Some(dir) => Path::new(dir),
+                None => input_file.parent().unwrap()
+            };
+            let output_file = output_dir.join(input_file.with_extension("TXT").file_name().unwrap());
+
+            let sexp = fs::read_to_string(&input_file)?;
+            let scene = disasm::assemble(&sexp)?;
+
+            let mut file = File::create(&output_file)?;
+            scene.write(&mut file)?;
+
+            println!("Wrote to {:?}.", output_file);
+        },
+        _ => println!("{}", matches.usage())
+    }
 
     Ok(())
 }
--- avg32/src/lib.rs
+++ avg32/src/lib.rs
@@ -4,6 +4,7 @@ extern crate serde;
 #[macro_use] extern crate serde_derive;
 extern crate encoding_rs;
 extern crate byteorder;
+extern crate anyhow;
 
 #[cfg(test)]
 #[macro_use] extern crate pretty_assertions;
@@ -11,37 +12,27 @@ extern crate byteorder;
 pub mod parser;
 pub mod write;
 
-pub use parser::AVG32Scene;
 use std::fs::File;
 use std::io::Read;
+use anyhow::{anyhow, Result};
 
-pub fn load<T: AsRef<str>>(filename: &T) -> Result<AVG32Scene, &'static str> {
-    println!("{}", filename.as_ref());
+pub use parser::AVG32Scene;
+
+pub fn load<T: AsRef<str>>(filename: &T) -> Result<AVG32Scene> {
     match File::open(filename.as_ref()) {
         Ok(mut f) => {
             let mut buffer = Vec::new();
             f.read_to_end(&mut buffer).expect("Unable to read file");
             load_bytes(&buffer)
         }
-        Err(_) => Err("Unable to load file"),
+        Err(e) => Err(anyhow!("Unable to load file: {}", e)),
     }
 }
 
-pub fn load_bytes(bytes: &[u8]) -> Result<AVG32Scene, &'static str> {
+pub fn load_bytes(bytes: &[u8]) -> Result<AVG32Scene> {
     let res = match parser::avg32_scene(bytes) {
         Ok((_, parsed)) => Ok(parsed),
-        Err(_) => Err("Not a valid AVG32 scene"),
-    };
-
-    print_trace!();
-
-    res
-}
-
-pub fn load_ops(bytes: &[u8]) -> Result<Vec<parser::Opcode>, &'static str> {
-    let res = match parser::opcodes(bytes) {
-        Ok((_, parsed)) => Ok(parsed),
-        Err(_) => Err("Not a valid AVG32 scene"),
+        Err(e) => Err(anyhow!("Not a valid AVG32 scene: {}", e)),
     };
 
     print_trace!();
